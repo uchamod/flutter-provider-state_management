60e1ad2919568880283cc815f38752ef90450188
chamod udara <uchamod52@gmail.com>
2024-07-06 13:23:12 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+//this is state management demo project uisng provider
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
